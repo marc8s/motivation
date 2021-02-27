0ae3668c49b6f2afbbf80b5c7633d3e4f8a0f9d9
Marc8s <marcus.ecomp@gmail.com>
2021-02-27 17:41:25 +0000

    Splash Activity

---
+++ app/src/main/java/com/example/motivation/SplashActivity.kt
@@ -0,0 +1,16 @@
+package com.example.motivation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        //esconder barra de suporte
+        if(supportActionBar != null){
+            supportActionBar!!.hide()
+        }
+    }
+}
