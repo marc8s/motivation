d98860b9b319385993dbf3c71a3e98612c083df9
Marc8s <marcus.ecomp@gmail.com>
2021-03-01 10:43:34 +0000

    Eventos de click SplahActivity

--- app/src/main/java/com/example/motivation/SplashActivity.kt
+++ app/src/main/java/com/example/motivation/SplashActivity.kt
@@ -1,9 +1,13 @@
 package com.example.motivation
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_splash.*
 
-class SplashActivity : AppCompatActivity() {
+class SplashActivity : AppCompatActivity(), View.OnClickListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
@@ -11,6 +15,24 @@ class SplashActivity : AppCompatActivity() {
         //esconder barra de suporte
         if(supportActionBar != null){
             supportActionBar!!.hide()
+        }
+
+        buttonSave.setOnClickListener(this);
+    }
+
+    override fun onClick(view: View) {
+        val id = view.id
+        if(id == R.id.buttonSave){
+            handleSave()
+        }
+    }
+
+    private fun handleSave(){
+        val name = editName.text.toString()
+        if(name!=""){
+            startActivity(Intent(this, MainActivity::class.java))
+        }else{
+            Toast.makeText(this, getString(R.string.informenome), Toast.LENGTH_SHORT).show()
         }
     }
 }
